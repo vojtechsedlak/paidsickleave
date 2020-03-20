333a394c253c40a5832e28ebc3c8a1e1dcd412b8
Vojtech Sedlak <vojtech@sumofus.org>
2020-03-20 09:55:08 -0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+	console.log("yeehaaa");
+	$('#dots').show();
+    $.ajax({url: "https://spreadsheets.google.com/feeds/list/1f5MsqVo264-wDyptNJt9j_jEP9dwWXkRuJbukicIqW4/default/public/values?alt=json", success: function(result){
+        console.log(result);
+        var data = result.feed.entry;
+        for (var i=0;i<data.length;i++) {
+            var name = data[i].gsx$company.$t;
+            var employees = data[i].gsx$employees.$t;
+        	var logo = data[i].gsx$image.$t;
+        	var handle = encodeURI(data[i].gsx$twitter.$t);
+        	var tweet = encodeURI('.'+handle+': Withdraw your support for @FairPlayCanada. This ineffective Internet censorship proposal will harm consumers, innovation and free expression online (cc: @CRTCeng @NavdeepSBains) https://unfairplay.ca')
+        	$('#board').append('<div class="entity col-3"><div class="entity-image"><img src="img/logos/'+logo+'"></div><p><strong>'+name+'</strong></p><a target="_new" onClick="gtag(\'event\', \'Tweet Click\')" class="btn" href="https://twitter.com/intent/tweet?text='+tweet+'&hashtags=DontCensor"><img src="img/twitter_white.svg" style="width:20px;"> Tweet</a></div>');
+        }
+        $('#dots').hide();
+    }});
+})
